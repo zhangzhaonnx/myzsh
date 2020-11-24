a7b877895754a282d37293835201aeddd9307db8
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-11-24 11:36:23 +0800

    first commit

--- alias.zsh
+++ alias.zsh
@@ -1,8 +1,30 @@
 alias c='clear'
+
+# ls
+alias ls='ls -G'
 alias ll='ls -lFG'
+alias l.='ls -lFG -d .*'
+
+# cd
+alias cd..='cd ..'
+alias ..='cd ..'
+alias .2='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+alias bc='bc -l'
+alias sha1='openssl sha1'
+alias mkdir='mkdir -pv'
+alias md='mkdir -pv'
+alias diff='colordiff'
+alias mount='mount |column -t'
+alias h='history | less +G'
+alias jobs='jobs -l'
 alias ping='ping -c 5'
+alias vi='vim'
+
 alias k9='kill-9'
-alias hi='history | less +G'
 alias ipe='curl ipinfo.io/ip'
 alias ipi='ipconfig getifaddr en0'
 alias k=kubectl
--- function.zsh
+++ function.zsh
@@ -1,3 +1,38 @@
 bashman() {
 	man bash | less -p"^       ${1} "
 }
+
+fg() {
+    if [[ $# -eq 1 && $1 = - ]]; then
+        builtin fg %-
+    else
+        builtin fg %"$@"
+    fi
+}
+
+bg() {
+    if [[ $# -eq 1 && $1 = - ]]; then
+        builtin bg %-
+    else
+        builtin bg %"$@"
+    fi
+}
+
+disown() {
+    if [[ $# -eq 1 && $1 = - ]]; then
+        builtin disown %-
+    else
+        builtin disown %"$@"
+    fi
+}
+
+acp() {
+	git add .
+	git commit -m 'add gtb comments'
+	git push -u origin comments
+}
+
+ac() {
+    git add .
+	git commit -m "${1:-first commit}"
+}
