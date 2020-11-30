15495a3d52db16e55a2833c7e76a27852cbc7318
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-11-30 17:50:18 +0800

    change

--- alias.zsh
+++ alias.zsh
@@ -23,6 +23,7 @@ alias h='history | less +G'
 alias jobs='jobs -l'
 alias ping='ping -c 5'
 alias vi='vim'
+alias gl='glol'
 
 alias k9='kill-9'
 alias ipe='curl ipinfo.io/ip'
@@ -30,3 +31,14 @@ alias ipi='ipconfig getifaddr en0'
 alias k=kubectl
 alias kcd='kubectl config set-context $(kubectl config current-context) --namespace'
 alias local.parallel='ssh parallels@10.211.55.3'
+
+# docker
+alias sq.u='docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:latest'
+alias sq.d='docker rm -f sonarqube'
+alias nexus.u='docker run -d -p 8081:8081 --name nexus sonatype/nexus:oss'
+alias nexus.d='docker rm -f nexus'
+alias jenkins.u='docker run -d --name jenkins -u root -p 9090:8080 -p 50000:50000 -v $(which docker):/usr/bin/docker -e TZ="Asia/Shanghai" -v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts'
+alias jenkins.d='docker rm -f jenkins'
+alias dind.u='docker run --privileged --name my-docker -d docker:dind'
+alias dm='docker-machine'
+alias dmc='docker-machine create -d virtualbox'
--- function.zsh
+++ function.zsh
@@ -26,13 +26,48 @@ disown() {
     fi
 }
 
-acp() {
+gtb.acp() {
 	git add .
 	git commit -m 'add gtb comments'
 	git push -u origin comments
 }
 
+acp() {
+    local message=${1:?commit message is required!}
+    git add .
+	git commit -m "$message"
+    git push
+}
+
 ac() {
     git add .
 	git commit -m "${1:-first commit}"
+}
+
+# maven
+mvng() {
+    local artifactId=${1:?artifact id is required!}
+    local groupId=${2:-com.example}
+    mvn archetype:generate \
+    -DgroupId="$groupId" \
+    -DartifactId="$artifactId" \
+    -DarchetypeArtifactId=maven-archetype-quickstart \
+    -DinteractiveMode=false \
+    -DarchetypeCatalog=internal
+}
+
+mvnphaseg() {
+    local phase=${1:?plugin id is required!}
+    mvn help:describe -Dcmd="$phase"
+}
+
+mvnpluging() {
+    local plugin=${1:?plugin id is required!}
+    mvn "$plugin":help
+}
+
+mvnplugingd() {
+    local plugin=${1:?plugin id is required!}
+    local goal=${2:?goal id is required!}
+    mvn help:describe -DartifactId=maven-"$plugin"-plugin -DgroupId=org.apache.maven.plugins -Dgoal="$goal" -Ddetail
 }
